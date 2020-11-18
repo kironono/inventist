472603e95ee13873e2e6230299b3d776ef2e16ef
Junichi Kakisako <kilo.sw@gmail.com>
2020-11-18 22:51:52 +0900

    Added product name validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
 
   enum status: {in_stock: 'in_stock', few_in_stock: 'few_in_stock', out_of_stock: 'out_of_stock'}
 
+  validates :name, length: { maximum: 64 }
   validates :status, inclusion: { in: statuses.keys }
 
 end
